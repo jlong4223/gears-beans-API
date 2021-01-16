1daa77da4e1861fce8c4fe7a5083cffd8bfb1e4d
Jared Long <jlong4223@gmail.com>
2021-01-16 13:14:24 -0600

    adds delete and show by id controllers/routes for services

--- controllers/services.js
+++ controllers/services.js
@@ -3,6 +3,8 @@ const BikeService = require("../models/service");
 module.exports = {
   showServices,
   createService,
+  showOneService,
+  deleteService,
 };
 
 // TODO set up other crud functions
@@ -13,11 +15,38 @@ async function showServices(req, res) {
   res.json(services);
 }
 
+async function showOneService(req, res) {
+  try {
+    const oneService = await BikeService.find({ _id: req.params.id });
+    res.json(oneService);
+  } catch (err) {
+    res.json(err);
+  }
+}
+
 async function createService(req, res) {
   try {
     await BikeService.create(req.body);
     showServices(req, res);
   } catch (err) {
     res.json({ err });
+  }
+}
+
+// TODO change to findByIdAndDelete? this does work though
+async function deleteService(req, res) {
+  try {
+    const deleteIt = await BikeService.findByIdAndRemove(req.params.id);
+    res.json(deleteIt);
+  } catch (err) {
+    res.json(err);
+  }
+}
+
+async function editService(req, res) {
+  try {
+    const updateService = await BikeService.findByIdAndUpdate;
+  } catch (err) {
+    res.json(err);
   }
 }
--- routes/services.js
+++ routes/services.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const serviceCtrl = require("../controllers/services");
 
 router.get("/services", serviceCtrl.showServices);
+router.get("/services/:id", serviceCtrl.showOneService);
 router.post("/services", serviceCtrl.createService);
-
+router.delete("/services/:id", serviceCtrl.deleteService);
 module.exports = router;
