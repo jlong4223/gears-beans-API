9e0e6c8cf323d2ec8040ebc299430dd9bf8c0916
Jared Long <jlong4223@gmail.com>
2021-02-09 22:27:47 -0600

    shows one review controller

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -3,11 +3,21 @@ const Review = require("../models/review");
 module.exports = {
   showReviews,
   createReview,
+  showOneReview,
 };
 
 async function showReviews(req, res) {
   const reviews = await Review.find({});
   res.json(reviews);
+}
+
+async function showOneReview(req, res) {
+  try {
+    const oneReview = await Review.findById(req.params.id);
+    res.json(oneReview);
+  } catch (err) {
+    res.json(err);
+  }
 }
 
 async function createReview(req, res) {
