393bfd545658cbe2741bf1cfaf107d983509d659
Jared Long <jlong4223@gmail.com>
2021-02-09 22:41:47 -0600

    adds update reviews route

--- routes/reviews.js
+++ routes/reviews.js
@@ -5,6 +5,7 @@ const reviewsCtrl = require("../controllers/reviews");
 router.get("/reviews", reviewsCtrl.showReviews);
 router.get("/reviews/:id", reviewsCtrl.showOneReview);
 router.post("/reviews", reviewsCtrl.createReview);
+router.put("/reviews/:id", reviewsCtrl.editReview);
 router.delete("/reviews/:id", reviewsCtrl.deleteReview);
 
 module.exports = router;
