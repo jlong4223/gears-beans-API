6c7dd01dc22049a30f1417a2032d27344b92a7b9
Jared Long <jlong4223@gmail.com>
2021-02-09 22:27:59 -0600

    adds route for each review id

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const reviewsCtrl = require("../controllers/reviews");
 
 router.get("/reviews", reviewsCtrl.showReviews);
+router.get("/reviews/:id", reviewsCtrl.showOneReview);
 router.post("/reviews", reviewsCtrl.createReview);
 
 module.exports = router;
