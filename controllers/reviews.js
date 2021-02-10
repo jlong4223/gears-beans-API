11063b3fab43f540cb9837ccce06c68aa003407b
Jared Long <jlong4223@gmail.com>
2021-02-09 22:35:05 -0600

    adds delete functionality

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -4,6 +4,7 @@ module.exports = {
   showReviews,
   createReview,
   showOneReview,
+  deleteReview,
 };
 
 async function showReviews(req, res) {
@@ -24,6 +25,15 @@ async function createReview(req, res) {
   try {
     await Review.create(req.body);
     showReviews();
+  } catch (err) {
+    res.json(err);
+  }
+}
+
+async function deleteReview(req, res) {
+  try {
+    const deleteIt = await Review.findByIdAndRemove(req.params.id);
+    res.json(deleteIt);
   } catch (err) {
     res.json(err);
   }
